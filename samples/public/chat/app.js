c095d6c6e9f462a3e8cb9c5c2e98f4b0a586b6e0
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-22 22:44:13 -0400

    init

--- samples/public/chat/app.js
+++ samples/public/chat/app.js
@@ -1,13 +1,45 @@
-const OPENAI_KEY = "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";
+const OPENAI_KEY = "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";
 const price = 0.0002/1000;
 
-const messages = [];
+const messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant that speaks in a funny way."
+    },
+];
 let totalTokens = 0;
 
 async function sendChat() {
     const prompt = document.querySelector("#prompt").value;
     document.querySelector("#prompt").value = "";
-   
+    document.querySelector("ul").innerHTML += `<li><b>${prompt}</b></li>`
+
+    messages.push({
+        "role": "user",
+        "content": prompt
+      })
+
+    const data = {
+        "model": "gpt-3.5-turbo",
+        "temperature": 0,
+        messages
+    }
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions",
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPENAI_KEY}`
+        },
+        body: JSON.stringify(data)
+    })
+
+    const json = await response.json();
+    messages.push(json.choices[0].message);
+    const message = json.choices[0].message.content;
+    document.querySelector("ul").innerHTML += `<li>${message}</li>`
+
     // TODO make query and parse results
 
     document.querySelector("#prompt").value = "";
--- samples/server.js
+++ samples/server.js
@@ -1,6 +1,6 @@
+const { Configuration, OpenAIApi } = require("openai");
 
-
-const OPENAI_KEY = "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";
+const OPENAI_KEY = "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";
 
 const path = require('path');
 const express = require('express');
@@ -13,11 +13,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.post('/api/chat', async (req, res) => {
-  
+  const body = req.body;
+  const response = await fetch("https://api.openai.com/v1/chat/completions",
+    {
+        method: 'POST',
+        temperature: 0,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPENAI_KEY}`
+        },
+        body: JSON.stringify(data)
+    })
+
+    const json = await response.json();
+    res.json(await response.json());
 });
 
+const configuration = new Configuration({
+  apiKey: OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 app.post('/api/general', async (req, res) => {
-  
+  const body = req.body;
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: body.prompt }],
+  });
+
+  res.json(completion.data.choices[0].message.content);
 });
 
 app.post('/api/image', async (req, res) => {
